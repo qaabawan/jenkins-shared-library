53e9d4b6f8397c449994deab279fcc772b7da3ae
kaabmalik <qaabawan@gmail.com>
2026-05-19 23:36:09 +0500

    fix: remove citation artifacts from devPipeline

--- vars/devPipeline.groovy
+++ vars/devPipeline.groovy
@@ -1,10 +1,11 @@
 def call() {
-    
-    echo 'Running Dev Stage: Data Ingestion and Training...' [cite: 12, 13]
+
+    echo 'Running Dev Stage: Data Ingestion and Training...'
     sh '''
         cd ~/mlops_assignment
         source venv/bin/activate
         python train.py
     '''
-    echo 'Model evaluation completed successfully. Assigned alias: Challenger.' [cite: 16, 20]
+
+    echo 'Model evaluation completed successfully. Assigned alias: Challenger.'
 }
